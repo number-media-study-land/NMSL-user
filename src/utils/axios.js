eb9a056c5626beb4aa2fb13f43bde41e74f471a3
ParaSLee <610033941@qq.com>
2019-04-13 20:28:04 +0800

    修改后端host

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+let net = "119.27.174.34";
+// let net = "localhost";
+
 const instance = axios.create({
-  baseURL: `http://${process.env.HOST || "localhost"}:${process.env.PORT ||
-    3000}`,
+  baseURL: `http://${net}:${process.env.PORT || 3000}`,
   timeout: 20000,
   withCredentials: true,
   transformResponse(res) {
